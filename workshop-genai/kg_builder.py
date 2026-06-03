90a8bb21ffa96cdffa3155a409a25b5b27091c45
Peter L <49878812+Peter-78@users.noreply.github.com>
2026-06-03 09:42:52 +0000

    första steg

--- workshop-genai/kg_builder.py
+++ workshop-genai/kg_builder.py
@@ -8,6 +8,9 @@ from neo4j import GraphDatabase
 from neo4j_graphrag.llm import OpenAILLM
 from neo4j_graphrag.embeddings import OpenAIEmbeddings
 from neo4j_graphrag.experimental.pipeline.kg_builder import SimpleKGPipeline
+from neo4j_graphrag.experimental.components.text_splitters.fixed_size_splitter import FixedSizeSplitter
+
+text_splitter = FixedSizeSplitter(chunk_size=500, chunk_overlap=100)
 
 neo4j_driver = GraphDatabase.driver(
     os.getenv("NEO4J_URI"),
@@ -32,6 +35,7 @@ kg_builder = SimpleKGPipeline(
     neo4j_database=os.getenv("NEO4J_DATABASE"), 
     embedder=embedder, 
     from_pdf=True,
+    text_splitter=text_splitter,
 )
 
 pdf_file = "./workshop-genai/data/genai-fundamentals_1-generative-ai_1-what-is-genai.pdf"
